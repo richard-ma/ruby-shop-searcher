6b6bb0f2de5c10c05fe711a140928382cf2bd353
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-21 21:55:42 +0800

    implement baidu_default_parser

--- ruby-shop-searcher.rb
+++ ruby-shop-searcher.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require "open-uri"
+require "rubygems"
+require "hpricot"
 
 keywords_file = './keywords'
 result_dir = '/tmp/rubyShopSearcher'
@@ -21,11 +23,13 @@ end
 # Search tools
 # Bing
 def bing keyword, start_record = 1, parser = method(:bing_default_parser)
-    ["http://global.bing.com/search?q=#{keyword}&first=#{start_record}", parser]
+    ["http://global.bing.com/search?q=#{keyword}+language%3Aen&first=#{start_record}", parser]
 end
 
 def bing_default_parser html
-    p 'bing_parser'
+    html.search("#b_results/li/h2/a").each do |result|
+        p result.attributes['href']
+    end
 end
 
 # Baidu
@@ -35,7 +39,18 @@ def baidu keyword, start_record = 1, parser = method(:baidu_default_parser)
 end
 
 def baidu_default_parser html
-    p 'baidu_parser'
+    html.search("div.result/h3/a").each do |result|
+        baidu_short_link = result.attributes['href']
+        begin
+            site = open(baidu_short_link, {
+                "User-Agent" => "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0",
+            })
+            p site.base_uri.to_s
+        rescue => e
+            p baidu_short_link
+            next
+        end
+    end
 end
 
 # Search bot engine
@@ -43,19 +58,15 @@ def search obj
     url, parser = obj
 
     html = nil
-    #begin
-        open url do |f|
-            html = f.read
-            parser.call html
-        end
-    #rescue StandardError, Timeout::Error, SystemCallError, Errno::ECONNREFUSED
-        #p $!
-    #else
-        #p url
-    #end
+    begin
+        html = open(url) {|f| Hpricot(f)} # get html and use Hpricot parsing
+        parser.call html
+    rescue => e
+        # nothing to do
+    end
 
     html
 end
 
 search bing 'hello'
-search baidu 'hello'
+search baidu 'china'
