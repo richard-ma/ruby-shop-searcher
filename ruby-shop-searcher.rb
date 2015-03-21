2f81317052534f9e2f5f88b1d6d6d815ba952f52
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-21 17:02:39 +0800

    add result parser

--- ruby-shop-searcher.rb
+++ ruby-shop-searcher.rb
@@ -18,30 +18,44 @@ def read_keywords_from keywords_file
     keywords
 end
 
+# Search tools
 # Bing
-def bing keyword, start_record = 1
-    "http://global.bing.com/search?q=#{keyword}&first=#{start_record}"
+def bing keyword, start_record = 1, parser = method(:bing_parser)
+    ["http://global.bing.com/search?q=#{keyword}&first=#{start_record}", parser]
+end
+
+def bing_parser html
+    p 'bing_parser'
 end
 
 # Baidu
-def baidu keyword, start_record = 1
+def baidu keyword, start_record = 1, parser = method(:baidu_parser)
     start_record = start_record / 10
-    "http://www.baidu.com/s?wd=#{keyword}&pn=#{start_record}"
+    ["http://www.baidu.com/s?wd=#{keyword}&pn=#{start_record}", parser]
 end
 
-def search url
+def baidu_parser html
+    p 'baidu_parser'
+end
+
+# Search bot engine
+def search obj
+    url, parser = obj
+
     html = nil
-    begin
+    #begin
         open url do |f|
             html = f.read
+            parser.call html
         end
-    rescue StandardError, Timeout::Error, SystemCallError, Errno::ECONNREFUSED
-        p $!
-    else
-        p url
-    end
+    #rescue StandardError, Timeout::Error, SystemCallError, Errno::ECONNREFUSED
+        #p $!
+    #else
+        #p url
+    #end
 
     html
 end
 
-p search baidu 'hello'
+search bing 'hello'
+search baidu 'hello'
