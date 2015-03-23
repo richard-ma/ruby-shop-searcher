ddcc1663fae349046de23330856d203c18883691
Richard Ma <richard.ma.19850509@gmail.com>
2015-03-23 13:35:22 +0800

    add this file to sort and uniq sites file

---
+++ pre_process_sites_file.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+tempfile=/tmp/sites-temp-file
+
+SAVEIFS=$IFS
+IFS=$"\n"
+
+if [ $# -ne 1 ]; then
+    echo "Please input a directory path"
+    exit 1
+fi
+
+files=$1/*.sites
+
+for file in $files; do
+    # sort and uniq
+    sort "$file" | uniq > "$tempfile"
+
+    # move back
+    rm "$file"
+    mv "$tempfile" "$file.checked"
+
+    echo "$file completed."
+done
+
+IFS=$SAVEIFS
+
+echo "All files completed."
